bc0dcc9baaa6c4a10ef42c946a2ff8425ef8216f
Superbil <superbil@gmail.com>
2012-11-22 17:47:57 +0800

    undo-tree don't need to use eval-after-load

--- init-undo-tree.el
+++ init-undo-tree.el
@@ -1,10 +1,8 @@
-(eval-after-load "undo-tree"
-  '(progn
-     (require 'undo-tree)
-     (global-undo-tree-mode)
+(require 'undo-tree)
+(global-undo-tree-mode)
 
-     ;; Represent undo-history as an actual tree (visualize with C-x u)
-     (setq undo-tree-mode-lighter "")))
+;; Represent undo-history as an actual tree (visualize with C-x u)
+(setq undo-tree-mode-lighter "")
 
 
 (provide 'init-undo-tree)
