3c8d7e345f61f605f696141a0d16c47b5f2f7a3f
Superbil <superbil@gmail.com>
2013-03-05 17:52:42 +0800

    setup twittering-mode hotkey

--- init-editing-utils.el
+++ init-editing-utils.el
@@ -322,7 +322,7 @@ With arg N, insert N newlines."
 ;;----------------------------------------------------------------------------
 ;; turn on/off truncate-lines
 ;;----------------------------------------------------------------------------
-(global-set-key (kbd "C-x t") 'toggle-truncate-lines)
+(global-set-key (kbd "C-x T") 'toggle-truncate-lines)
 
 ;;----------------------------------------------------------------------------
 ;; Use shell-like backspace C-h, rebind help
