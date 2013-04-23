249be86feb529708a3e71d6e23084099ce04e06d
Superbil <superbil@gmail.com>
2013-04-23 10:56:55 +0800

    twitting-mode hotkey for favorite/unfavorite

--- init-twittering.el
+++ init-twittering.el
@@ -3,6 +3,10 @@
 ;; Avoid to setup password each times
 (setq twittering-use-master-password t)
 
+;; favorite/unfavorite hotkey
+(define-key twittering-mode-map (kbd "C-c f") 'twittering-favorite)
+(define-key twittering-mode-map (kbd "C-c F") 'twittering-unfavorite)
+
 ;; setup hotkey
 (global-set-key (kbd "C-x t") 'twit)
 
