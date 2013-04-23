cd05305f47ce33aabbf32365e0f0dd24fa19e1d0
Superbil <superbil@gmail.com>
2013-04-23 11:11:29 +0800

    let "C-x p" to quick change function
    this function work just like use ^ in Xcode

--- init-objc.el
+++ init-objc.el
@@ -1,3 +1,22 @@
 (add-auto-mode 'objc-mode "\\.m$" "\\.mm$")
 
+(require-package 'anything)
+(require-package 'anything-config)
+
+(defvar anything-c-source-objc-headline
+  '((name . "Objective-C Headline")
+    (headline  "^[-+@]\\|^#pragma mark")))
+
+(defun objc-headline ()
+  (interactive)
+  ;; Set to 500 so it is displayed even if all methods are not narrowed down.
+  (let ((anything-candidate-number-limit 500))
+    (anything-other-buffer '(anything-c-source-objc-headline)
+                           "*ObjC Headline*")))
+
+(defun objc-keys ()
+  (local-set-key (kbd "C-x p") 'objc-headline))
+
+(add-hook 'objc-mode-hook 'objc-keys)
+
 (provide 'init-objc)
