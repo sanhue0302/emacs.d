eab019543cc794df97b20de6631e3e214c46c7c6
Superbil <superbil@gmail.com>
2013-04-24 14:04:17 +0800

    fix init-twittering
        * fix require way
        * change status-format

--- init-twittering.el
+++ init-twittering.el
@@ -1,4 +1,4 @@
-(require-package 'twittering-mode)
+(require 'twittering-mode)
 
 ;; Avoid to setup password each times
 (setq twittering-use-master-password t)
@@ -9,5 +9,8 @@
 
 ;; setup hotkey
 (global-set-key (kbd "C-x t") 'twit)
+
+;; setup status-format
+(twittering-update-status-format "%i %S @%s,  %@:\n%FILL{  %T // from %f%L%r%R}\n")
 
 (provide 'init-twittering)
