42bb06e99db55f33bf069c2a096d677021678619
Superbil <superbil@gmail.com>
2013-04-30 17:29:42 +0800

    set prompt for eshell

--- init-sh.el
+++ init-sh.el
@@ -16,6 +16,22 @@
              (eshell/export "EDITOR=emacsclient -n")
              (eshell/export "VISUAL=emacsclient -n")))
 
+;;; set prompt for eshell
+(defmacro with-face (str &rest properties)
+    `(propertize ,str 'face (list ,@properties)))
+
+(defun shk-eshell-prompt ()
+  (concat
+   (abbreviate-file-name
+    (eshell/pwd))
+   (when (magit-get-current-branch)
+     (with-face (format " (%s)" (magit-get-current-branch)) :foreground "blue"))
+   (if (= (user-uid) 0)
+       (with-face " #" :foreground "red")
+     " $")
+   " "))
+(setq eshell-prompt-function 'shk-eshell-prompt)
+
 ;;; Start eshell or switch to it if it's active.
 (global-set-key (kbd "C-x m") 'eshell)
 ;;; Change default mail to "C-x M"
