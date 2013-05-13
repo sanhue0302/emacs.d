97ff6a516d90805d1dbac90915d10948d0ffc325
Superbil <superbil@gmail.com>
2013-05-13 23:19:35 +0800

    use same function to define-key

--- init-dired.el
+++ init-dired.el
@@ -1,19 +1,16 @@
 (require-package 'dired+)
 
-(eval-after-load 'dired
-  '(progn
-     (require 'dired+)
-     (setq dired-recursive-deletes 'top)
-     (define-key dired-mode-map [mouse-2] 'dired-find-file)))
-
 (defun dired-show-only (regexp)
    (interactive "sFiles to show (regexp): ")
    (dired-mark-files-regexp regexp)
    (dired-toggle-marks)
    (dired-do-kill-lines))
 
-(eval-after-load 'dired+
+(eval-after-load 'dired
   '(progn
+     (require 'dired+)
+     (setq dired-recursive-deletes 'top)
+     (define-key dired-mode-map [mouse-2] 'dired-find-file)
      (define-key dired-mode-map (kbd "/") 'dired-show-only)))
 
 (provide 'init-dired)
