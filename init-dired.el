0bddd329d827d6c654307ecc95485a731fa28f01
Superbil <superbil@gmail.com>
2013-05-13 10:20:41 +0800

    add regexp filter function on dired-mode

--- init-dired.el
+++ init-dired.el
@@ -8,4 +8,12 @@
   (setq dired-recursive-deletes 'top)
   (define-key dired-mode-map [mouse-2] 'dired-find-file))
 
+(defun dired-show-only (regexp)
+   (interactive "sFiles to show (regexp): ")
+   (dired-mark-files-regexp regexp)
+   (dired-toggle-marks)
+   (dired-do-kill-lines))
+
+(define-key dired-mode-map (kbd "/") 'dired-show-only)
+
 (provide 'init-dired)
